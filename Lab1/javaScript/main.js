cb3bc81aaee0cd610c9b36c422f26111e27d5c56
JoeManyak <Doncrafttv@gmail.com>
2020-10-15 12:47:33 +0300

    Comments2

--- Lab1/javaScript/main.js
+++ Lab1/javaScript/main.js
@@ -1,9 +1,9 @@
 function calculate() {
-    let a = parseFloat(document.getElementById("inp1").value);
-    let b = parseFloat(document.getElementById("inp2").value);
-    let c = parseFloat(document.getElementById("inp3").value);
-    let r = parseFloat(document.getElementById("inp4").value);
-    if (a + b + c === 180) {
+    let a = parseFloat(document.getElementById("inp1").value); //Зчитування міри кута а
+    let b = parseFloat(document.getElementById("inp2").value); //Зчитування міри кута b
+    let c = parseFloat(document.getElementById("inp3").value); //Зчитування міри кута c
+    let r = parseFloat(document.getElementById("inp4").value); //Зчитування міри радіуса вписаного кола
+    if (a + b + c === 180) { //перевірка існування трикутника
         let side1 = sideCalculate(a, r);
         let side2 = sideCalculate(b, r);
         let side3 = sideCalculate(c, r);
@@ -13,12 +13,10 @@ function calculate() {
     }
 }
 
-function sideCalculate(a, r) {
-    console.log(r)
-    console.log(Math.sin((toRadian(a))));
+function sideCalculate(a, r) { // Функція розрахунку сторони за радіусом вписаного кола та кутом (т. синусів)
     return 2 * r * Math.sin((toRadian(a)));
 }
 
-function toRadian(angle) {
+function toRadian(angle) { // Функція переведення в радіани
     return (angle * Math.PI) / 180;
 }
