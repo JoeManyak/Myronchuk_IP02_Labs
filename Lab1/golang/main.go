42d5c6d78889567f55c1553ab559a82b18687456
JoeManyak <Doncrafttv@gmail.com>
2020-10-15 12:18:12 +0300

    Comments

--- Lab1/golang/main.go
+++ Lab1/golang/main.go
@@ -6,16 +6,16 @@ import (
 )
 
 func main() {
-	var a, b, c, r float64
+	var a, b, c, r float64 //a, b, c -- кути трикутника, r -- радіус вписаного кола
 	fmt.Print(">>> Вкажіть градусну міру кута a: ")
-	_, _ = fmt.Scan(&a)
+	_, _ = fmt.Scan(&a) //Зчитування
 	fmt.Print(">>> Вкажіть градусну міру кута b: ")
 	_, _ = fmt.Scan(&b)
 	fmt.Print(">>> Вкажіть градусну міру кута c: ")
 	_, _ = fmt.Scan(&c)
 	fmt.Print(">>> Вкажіть радіус вписаного кола: ")
 	_, _ = fmt.Scan(&r)
-	if a+b+c == 180 {
+	if a+b+c == 180 { //перевірка існування трикутника
 		side1 := sideCalculate(r, a)
 		side2 := sideCalculate(r, b)
 		side3 := sideCalculate(r, c)
@@ -25,8 +25,8 @@ func main() {
 	}
 }
 func toRadian(angle float64) float64 {
-	return (angle * math.Pi) / 180
+	return (angle * math.Pi) / 180 // Функція переведення в радіани
 }
 func sideCalculate(r, a float64) float64 {
-	return 2 * r * math.Sin(toRadian(a))
+	return 2 * r * math.Sin(toRadian(a)) // Функція розрахунку сторони за радіусом вписаного кола та кутом (т. синусів)
 }
